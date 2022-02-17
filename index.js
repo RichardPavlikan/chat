3b18153fd037a03be38443be7e279f118313635e
RichardPavlikan <68600674+RichardPavlikan@users.noreply.github.com>
2022-02-17 14:33:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+// import { initializeApp } from "http://www.gstatic.com/firebasejs/9.6.5/firebase-app.js";
+
+// LEARNING
+// možnost jak přiradit funkci k button
+// var btn = document.getElementById("btnTest");
+// btn.addEventListener("click", test(), false);
+
+
+/*
+import { initializeApp } from "http://www.gstatic.com/firebasejs/9.6.5/firebase-app.js";
+import { getDatabase, ref, set } from "http://www.gstatic.com/firebasejs/9.6.5/firebase-database.js";
+
+
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set } from "firebase/database";
+*/
+/*
+const firebaseConfig = {
+    apiKey: "AIzaSyDenUZfgI0Z5Vcw8v8PyxpkPx4cxxJK6TU",
+	authDomain: "schoolstuff-b9150.firebaseapp.com",
+	projectId: "schoolstuff-b9150",
+	storageBucket: "schoolstuff-b9150.appspot.com",
+	messagingSenderId: "492506681866",
+	appId: "1:492506681866:web:42dc31c7a69984688445f3"
+};
+	  
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+/*
+const db = getDatabase(app);
+
+set(ref(db), {
+    username: "ji",
+});
+
+*/
+
+function test()
+{document.write("test");}
